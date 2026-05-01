5a870f179f55dd447153a098dce54bfa3032a0ab
Spiros Halkias <spiro737halkias@gmail.com>
2026-05-01 13:26:05 -0400

    add button click to each Projects

--- Educational Projects/EducationalProjectsHome.js
+++ Educational Projects/EducationalProjectsHome.js
@@ -1,13 +1,19 @@
 const projects = [{
     image: 'EducationalProjectsHomeImages/robotproj.png',
-    button: 'accessrobot',
+    link: 'Autonomous Robot Project/AutonomousRobotProject.html',
     textone: 'Automonous Robot Project // December 6th, 2024',
     texttwo: 'This robot project focused on designing, building, and programming an autonomous robot to navigate a modular maze. As a team we defined the problem, created designs, and developed custom components using CAD tools. The final robot successfully met project goals, demonstrating effective navigation and modularity. This project emphasized teamwork, problem-solving, and hands-on engineering experience.',
     textthree: 'Size: 2 Person Group'
     },
     {
     image: 'EducationalProjectsHomeImages/robotproj.png',
-    button: 'accessrobot',
+    link: 'Autonomous Robot Project/AutonomousRobotProject.html',
+    textone: 'Automonous Robot Project // December 6th, 2024',
+    texttwo: 'This robot project focused on designing, building, and programming an autonomous robot to navigate a modular maze. As a team we defined the problem, created designs, and developed custom components using CAD tools. The final robot successfully met project goals, demonstrating effective navigation and modularity. This project emphasized teamwork, problem-solving, and hands-on engineering experience.',
+    textthree: 'Size: 2 Person Group'
+    },
+    {image: 'EducationalProjectsHomeImages/robotproj.png',
+    link: 'Autonomous Robot Project/AutonomousRobotProject.html',
     textone: 'Automonous Robot Project // December 6th, 2024',
     texttwo: 'This robot project focused on designing, building, and programming an autonomous robot to navigate a modular maze. As a team we defined the problem, created designs, and developed custom components using CAD tools. The final robot successfully met project goals, demonstrating effective navigation and modularity. This project emphasized teamwork, problem-solving, and hands-on engineering experience.',
     textthree: 'Size: 2 Person Group'
@@ -22,7 +28,7 @@ projects.forEach((projects) => {
     `<div class="projectbox-div">
         <div class="imgbutton-div">
             <img class="imgbutton-img" src="${projects.image}">
-            <button class="imgbutton-button" id="${projects.button}">
+            <button class="imgbutton-button" onclick="window.location.href = '${projects.link}'">
                 View Project
             </button>
         </div>
@@ -41,15 +47,6 @@ projects.forEach((projects) => {
 });
 
 document.querySelector('.projectscontainer-js').innerHTML = projectsHTML;
-
-
-
-
-
-document.getElementById('accessrobot').addEventListener('click', function() {
-    window.location.href = "Autonomous Robot Project/AutonomousRobotProject.html"
-    });
-
 
 document.getElementById('accesshome').addEventListener('click', function() {
     window.location.href = "../Home Page/landingpage.html"
