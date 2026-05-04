7d245c258a34363122f52b31ea38ed66608669ac
Spiros Halkias <spiro737halkias@gmail.com>
2026-05-04 15:46:50 -0400

    finished robot project and seacrh filter for Projs

---
+++ Educational Projects/Projectheaders.js
@@ -0,0 +1,7 @@
+document.getElementById('accesshome').addEventListener('click', function() {
+    window.location.href = "../../Home Page/landingpage.html"
+});
+
+document.getElementById('accessprojects').addEventListener('click', function() {
+    window.location.href = "../EducationalProjectsHome.html"
+});
