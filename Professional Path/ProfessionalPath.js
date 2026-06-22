a35dcf64c39315b71c51785b2b6ec794182fe9e6
Spiros Halkias <spiro737halkias@gmail.com>
2026-06-21 22:23:36 -0400

    fix buttons

--- Professional Path/ProfessionalPath.js
+++ Professional Path/ProfessionalPath.js
@@ -7,6 +7,9 @@ document.getElementById('accesshome2').addEventListener('click', function() {
 document.getElementById('university').addEventListener('click', function() {
     window.location.href = "University/UniversityHome.html"
 });
+document.getElementById('career').addEventListener('click', function() {
+    window.location.href = "Work/Career.html"
+});
 document.getElementById('openresume').addEventListener('click', function() {
     document.getElementById('resumehidden').classList.toggle('resumeshow')
 });
