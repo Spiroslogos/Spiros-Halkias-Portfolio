782eac115550a48c02946f2fabf1e89717ee812b
Spiros Halkias <spiro737halkias@gmail.com>
2026-07-14 21:04:01 -0400

    did professional work home page

--- Professional_Work/Professional_Work.js
+++ Professional_Work/Professional_Work.js
@@ -1,15 +1,9 @@
-document.getElementById('accesshome').addEventListener('click', function() {
-    window.location.href = "../Home Page/landingpage.html"
+document.getElementById('University').addEventListener('click', function() {
+    window.location.href = "University/University.html"
 });
-document.getElementById('accesshome2').addEventListener('click', function() {
-    window.location.href = "../Home Page/landingpage.html"
-});
-document.getElementById('university').addEventListener('click', function() {
-    window.location.href = "University/UniversityHome.html"
-});
-document.getElementById('career').addEventListener('click', function() {
+document.getElementById('Career').addEventListener('click', function() {
     window.location.href = "Work/Career.html"
 });
-document.getElementById('openresume').addEventListener('click', function() {
-    document.getElementById('resumehidden').classList.toggle('resumeshow')
+document.getElementById('Open_Resume').addEventListener('click', function() {
+    document.getElementById('Resume_Hidden').classList.toggle('Unhidden-div')
 });
